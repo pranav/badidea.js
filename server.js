a4a6e8903022a4cee5362d29c930474badf6308c
Pranav Gandhi <pranavrulz@gmail.com>
2013-02-16 08:45:30 +0000

    IT WORKS

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
 var http = require('http');
+var fs = require('fs');
 var i = 0;
+
 http.createServer(function(req, res) {
-  console.log("Got connection " + i++);
-  res.writeHead(200, {'Content-Type': 'text/javascript'});
-  res.end('console.log("LOL");');
-}).listen(8125, '0.0.0.0');
+  if(req.url == '/'){
+    fs.readFile('./index.html', function(error, content){
+      if(error) {
+        res.writeHead(500);
+        res.end();
+      } else {
+        res.writeHead(200, { 'Content-Type' : 'text/html' });
+        res.end(content, 'utf-8');
+      }
+    });
+  }
+  else if(req.url == "/server"){
+    res.writeHead(200, { 'Content-Type' : 'text/javascript' });
+    res.end('console.log("lolololol");');
+
+  }
+}).listen(80, '0.0.0.0');
 
 console.log("Running server");
