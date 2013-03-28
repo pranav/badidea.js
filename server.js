7181e412865d36570897862132c647dfce91c915
Pranav Gandhi <pranavrulz@gmail.com>
2013-03-28 15:55:55 +0000

    Added comments to server.js for the presentation

--- server.js
+++ server.js
@@ -3,26 +3,37 @@ var fs = require('fs');
 var i = 0;
 
 http.createServer(function(req, res) {
+
+  // Route for '/'
   if(req.url == '/'){
+    // Open file index.html
     fs.readFile('./index.html', function(error, content){
       if(error) {
         res.writeHead(500);
         res.end();
       } else {
+        // Send index.html to the client
         res.writeHead(200, { 'Content-Type' : 'text/html' });
         res.end(content, 'utf-8');
       }
     });
   }
-  else if(req.url == "/server"){
+
+  // Route for /getcat
+  else if(req.url == "/getcat"){
+    // Read scripts.js
     res.writeHead(200, { 'Content-Type' : 'text/javascript' });
     fs.readFile('./scripts.js', function(err, content){
+      // Pick a random line from scripts.js
       var options = (content + "").split("\n");
       var i = Math.floor((Math.random()*options.length));
+      // And send that line back to the client
       res.end(options[i]);
     });
 
   }
+
+  // 404 Error
   else {
     res.writeHead(404, { 'Content-Type' : 'text/html' });
     res.end('404. I don\'t have cookies for you');
